d7cd504777789c8a3022a0d47f7f00f14fd4e0a1
maxperozek <maxperozek@gmail.com>
2022-07-20 12:18:50 -0600

    a

--- transforms/wico_transforms.py
+++ transforms/wico_transforms.py
@@ -9,7 +9,7 @@ class WICOTransforms:
     def wico_5g_vs_non_conspiracy(wico):
         wico = [g for g in wico if g.y != 2]
         for i in range(len(wico)):
-            wico[i] = wico_data_to_custom(wico[i], MultiTargetData)
+            wico[i] = WICOTransforms.wico_data_to_custom(wico[i], MultiTargetData)
         return wico
     
     def wico_5g_vs_non_conspiracy_downsampled_balanced(wico):
@@ -44,6 +44,7 @@ class WICOTransforms:
 
     def wico_data_to_custom(data, custom_class):
         return custom_class(x=data.x, edge_index=data.edge_index, y=data.y)
+
 
 class MultiTargetData(Data):
     def __cat_dim__(self, key, value, *args, **kwargs):
